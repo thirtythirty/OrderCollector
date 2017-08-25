06e49fa4f07496911ac85796307955ebb253113f
thirtythirty <qawsedr443@gmail.com>
2017-08-25 09:17:24 +0900

    deviseを導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+   def after_sign_in_path_for(resource)
+        '/'
+    end
+
+    private
+    def sign_in_required
+      redirect_to new_user_session_url unless user_signed_in?
+    end
 end
--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,6 +1,7 @@
 class CollectionsController < ApplicationController
   before_action :set_collection, only: [:show, :edit, :update, :destroy]
 
+
   # GET /collections
   # GET /collections.json
   def index
