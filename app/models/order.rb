946edc93c381fa45c2b9cdac241446383debbbb3
thirtythirty <qawsedr443@gmail.com>
2017-08-30 07:16:22 +0900

    enumerize導入 orderのフォームをselectboxに

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :collection
 
-  enumerize :dish, in: [:wa, :ai], default: :wa
-  enumerize :rice, in: [:large, :medium, :small], default: :medium
-  enumerize :take_out, in: [:large, :medium, :small], default: :medium
+  enumerize :dish, in: [:wa, :ai, :none], default: :wa
+  enumerize :rice, in: [:large, :medium, :small,  :none], default: :medium
+  enumerize :take_out, in: [:large, :medium, :small, :none], default: :medium
 end
